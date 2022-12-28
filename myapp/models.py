37f947f0aaf18d37ab8d99f46456bc78734cf361
John Thomas <johnthomas@Johns-MacBook-Pro-M1.local>
2022-12-28 14:44:52 -0500

    first commit

---
+++ myapp/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    desc = models.CharField(max_length=200)
+    image = models.ImageField(blank=True, upload_to='images')
+    seller_name = models.ForeignKey(User,on_delete=models.CASCADE, default=1)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('myapp:products')
+
+class OrderDetail(models.Model):
+    customer_username = models.CharField(max_length=200)
+    product = models.ForeignKey(to='Product', on_delete=models.PROTECT)
+    amount = models.IntegerField()
+    stripe_payment_intent = models.CharField(max_length=200)
+    has_paid = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now_add=True)
+
---
+++ myapp/urls.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+app_name = 'myapp'
+
+urlpatterns = [
+    path('', views.index),
+    path('products/', views.products, name='products'),
+    # path('products/', views.ProductListView.as_view(), name='products'),
+    # path('products/<int:id>', views.product_detail, name='product_detail'),
+    path('products/<int:pk>', views.ProductDetailView.as_view(), name='product_detail'),
+    # path('products/add/', views.add_product, name='add_product'),
+    path('products/add/', views.ProductCreateView.as_view(), name='add_product'),
+    # path('products/update/<int:id>/', views.update_product, name='update_product'),
+    path('products/update/<int:pk>/', views.ProductUpdateView.as_view(), name='update_product'),
+    # path('products/delete/<int:id>/', views.delete_product, name='delete_product'),
+    path('products/delete/<int:pk>/', views.ProductDeleteView.as_view(), name='delete_product'),
+    path('products/mylistings/', views.my_listings, name='mylistings'),
+    path('success/', views.PaymentSuccessView.as_view(),name='success'),
+    path('failed/', views.PaymentFailedView.as_view(),name='failed'),
+    path('api/checkout-session/<id>',views.create_checkout_session,name='api_checkout_session'),
+]
